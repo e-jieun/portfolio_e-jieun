2f11cd48933b38471447ad7d47e5aff5acc93355
jieunlee <liz950827@gmail.com>
2022-05-28 15:02:52 +0900

    common root display changed, overflowX hidden 적용

--- js/module/common.js
+++ js/module/common.js
@@ -53,5 +53,5 @@ document.body.setAttribute(`style`,`margin: 0; padding: 0;`);
 const root = document.getElementById('root');
 console.log(root);
 // 가로 overflow를 컨트롤
-// root.style.overflowX = `hidden`;
-// setDisplay(root, `flex`);
+root.style.overflowX = `hidden`;
+setDisplay(root, `grid`);
