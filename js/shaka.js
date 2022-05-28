4e7d734b32a70b2941dd04674884ee223ff67951
jieunlee <liz950827@gmail.com>
2022-05-28 15:02:11 +0900

    footer icon display changed

--- js/shaka.js
+++ js/shaka.js
@@ -3,7 +3,10 @@ let hun = 100;
 const colorObj = {
   colorBk: `#000`,
   colorC4: `#c4c4c4`,
-  color33: `#333`
+  color33: `#333`,
+  colorNa: `#3773A5`,
+  colorFf: `#fff`,
+  colorSb: `rgba(186,216,242,1.0)`
 }
 // function
 const getAppendName = (element) => {
@@ -39,12 +42,15 @@ const shaka = document.getElementById('shaka');
 console.log(shaka);
 setSize(shaka, `${hun}vw`, `${hun}vh`);
 setDisplay(shaka, `grid`, `center`, `center`);
+setBgColor(shaka, `${colorObj.colorSb}`);
+shaka.style.color = `${colorObj.colorFf}`;
+
 // *shaka.chilren
 console.log(shaka.children);
 // 1. 모든 컨테이너의 아이템 가운데 정렬
 for(let i = 0; i < shaka.children.length; i++){
   setDisplay(shaka.children[i], `grid`);
-  setSize(shaka.children[i], `inherit`);
+  setSize(shaka.children[i], `inherit`, `${hun/2}%`);
 }
 // *shakahand
 const shakaHand = document.getElementById('shakahand');
@@ -60,5 +66,10 @@ for(let i = 0; i < footerText.children.length; i++){
   // 2. list-style-type: none;
   footerText.children[i].style.listStyleType = `none`;
 }
+
+// *#footericon => icon을 감싸고 있는 컨테이너
+const footerIcon = document.querySelector('#footericon');
+console.log(footerIcon);
+setDisplay(footerIcon, `flex`, `center`, `space-evenly`);
 
 // interaction 1. 손 마우스오버 이벤트 애니메이션
