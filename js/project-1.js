ac893d6d4c0b7c5b4839264385f940309ddb47ce
jieunlee <liz950827@gmail.com>
2022-06-10 16:21:37 +0900

    popuptest.html을 적용시켜서 popup을 끄고 켤 수 있도록 만들어주기

--- js/project-1.js
+++
@@ -1,9 +0,0 @@
-// import { setSize } from "/module/css-function";
-
-const projOne = document.getElementById('project-1');
-console.log(projOne);
-
-projOne.style.width = '100vw';
-projOne.style.height = '100vh';
-// setSize(projOne, '100vw', '100vh');
-// console.log('hi');
