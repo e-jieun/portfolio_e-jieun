daf4dfb7e9520530843f01966c7ae7a90b8796e4
jieunlee <liz950827@gmail.com>
2022-06-14 10:55:33 +0900

    getAppendName js 파일을 따로 만들어 줌

--- js/module/css-function.js
+++ js/module/css-function.js
@@ -1,11 +1,3 @@
-export const getAppendName = (element) => {
-  // 언제 가져오면 안되는가?
-  // null, 빈값일 때는 가져오면 안된다
-  if (element.tagName !== null && element.tagName !== '') {
-    return element.tagName;
-  }
-}
-
 export const setBgColor = (elem, bgColor) => {
   elem.style.backgroundColor = bgColor;
 }
