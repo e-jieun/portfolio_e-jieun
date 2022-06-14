91f0103ec00825427aad1c05d7513b8fa2f7b7f1
jieunlee <liz950827@gmail.com>
2022-06-14 10:52:40 +0900

    3차 디자인 수정 마크업 시작

--- js/module/cal-function.js
+++
@@ -1,8 +0,0 @@
-export const returnPercent = (elem, total, current) => {
-  // 현재의 값의 퍼센트를 출력해주는 공식
-  let result = (current/total)*100;
-  // 계산된 퍼센트를 리턴해준다
-  return result;
-}
-
-export default returnPercent;
