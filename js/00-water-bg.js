4f37a13dab4ba1d8123c5a291e695a04fc3ceded
jieunlee <liz950827@gmail.com>
2022-06-15 17:55:44 +0900

    water bg console removed

--- js/00-water-bg.js
+++ js/00-water-bg.js
@@ -5,13 +5,13 @@ import hun from "./module/reset.js";
 const root = document.getElementById('root');
 // *00-배경으로 사용될 water-bg 만들어 줄 부분
 makePage(root, 'section', 'water-bg');
-console.log(root.children);
+// console.log(root.children);
 
 // * waterBg
 const waterBg = document.getElementById('water-bg');
-console.log(waterBg);
+// console.log(waterBg);
 // *배경을 담을 상자 크기
-setSize(waterBg, `${hun}vw`, `${hun}vh`);
+setSize(waterBg, `${hun}vw`, `${hun*2}vh`);
 setPosition(waterBg, 'absolute', `${hun}vh`);
 waterBg.style.zIndex = -1; 
 waterBg.classList.add('bg-color');
