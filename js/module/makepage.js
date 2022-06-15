788e4b035467337ab39327549ed2a175c3614f6e
jieunlee <liz950827@gmail.com>
2022-06-15 15:32:28 +0900

    id default parameter made

--- js/module/makepage.js
+++ js/module/makepage.js
@@ -1,6 +1,6 @@
 // todo: 몇번이고 자식을 생성해줄 수 있도록 하는 함수를 만들어주면 좋을 것 같음
 // todo: 매번 재할당이 되는 구조로 했어서 문제가 있었다 
-const makePage = (parent, tagName, id) => {
+const makePage = (parent, tagName, id = '') => {
   const elem = document.createElement(tagName);
   elem.id = id;
   parent.appendChild(elem);
