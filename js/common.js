81c100292fe270673b9e48e2f713195466cd2e41
jieunlee <liz950827@gmail.com>
2022-06-16 23:20:45 +0900

    box-sizing

--- js/common.js
+++ js/common.js
@@ -13,8 +13,5 @@ const root = document.getElementById('root');
 console.log(root);
 
 // *root style
-// todo: overflowX: hidden; => 가로 overflow를 컨트롤
-// todo: box-sizing: border-box로 변경하는 편이 더 낫다
-// root.style.overflowX = `hidden`;
-// setDisplay(root, `grid`);
+root.style.boxSizing = 'border-box';
 // root.style.fontFamily = `'Roboto Slab', serif`;
