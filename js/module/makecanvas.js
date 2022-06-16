c261b047e028a310eb1bff9409b106d2be19091d
jieunlee <liz950827@gmail.com>
2022-06-16 17:33:12 +0900

    make canvas api setting size

--- js/module/makecanvas.js
+++ js/module/makecanvas.js
@@ -1,7 +1,5 @@
 const makeCanvas = (elem, id, width, height) => {
-  // if(typeof width === 'number' && typeof height === 'number'){
     elem.innerHTML = `<canvas id="${id}" width="${width}" height="${height}"></canvas>`
-  // }
 }
 
 export default makeCanvas;
