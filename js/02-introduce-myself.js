22f8859cb71765780ca6c3ac119db4a7f76e5de0
jieunlee <liz950827@gmail.com>
2022-06-20 17:43:00 +0900

    자기소개 css 작성하기

--- js/02-introduce-myself.js
+++ js/02-introduce-myself.js
@@ -5,6 +5,7 @@ import {
 import makePage from "./module/makepage.js";
 import makeElem from "./module/makeelem.js"
 import hun from "./module/reset.js";
+import colorObj from "./module/color.js";
 
 // *02-introduce-myself page 만들어 줄 부분
 const root = document.getElementById('root');
@@ -14,6 +15,7 @@ makePage(root, 'section', 'introduce-myself');
 // *myselfPage
 const myselfPage = document.getElementById('introduce-myself');
 // console.log(myselfPage);
+
 
 setSize(myselfPage, `${hun}vw`, `${hun*2}vh`);
 myselfPage.classList.add('border-bk');
@@ -53,6 +55,8 @@ const topMenuTextArr = ['Personal Page', 'Github'];
 // *topMenu > ul 각각 > li*2
 topMenuUl.forEach((elem, index) => {
   // console.log(elem);
+  elem.style.color = `${colorObj.colorSb}`;
+  elem.style.listStyleType = 'none';
   elem.innerHTML = `${makeElem('li')}${makeElem('li')}`;
   // console.log(elem);
   // elem.style.listStyleType = 'none';
@@ -77,31 +81,41 @@ myselfFontH1.innerHTML = `<div>안녕하세요</div>
 <span>새로운 시도</span>를 멈추지 않는</div>
 <div>웹 디자이너 <span>이지은</span>입니다</div>`;
 
-
 // todo: span을 제외한 부분에 classList .stroke font 적용 혹은 반대로
-// console.log(myselfFontH1.childNodes);
-// const h1ChildNodes = Array.from(myselfFontH1.childNodes);
-// console.dir(h1ChildNodes);
-// h1ChildNodes.forEach(elem => {elem.nodeName.toLowerCase === 'span'? console.log(elem): console.log('not span')});  // 다 not span이 뜬다
-// h1ChildNodes.forEach(elem => {
-//   // elem.classList.add('stroke-font');
-//   if(elem.childNodes.nodeName === 'SPAN'){
-//     console.log('hi');
-//     // elem.classList.remove('stroke-font');
-//   }
-// })
+const strokeText = Array.from(myselfFontH1.children);
+console.log(strokeText);
+strokeText.map(elem => {
+  elem.classList.add('stroke-font');
+  elem.style.fontSize = `3rem`;
+  elem.style.fontWeight = '800';
+});
+
+const fillText = document.querySelectorAll('span');
+const fillTextArr = Array.from(fillText);
+console.log(fillText);
+console.log(fillTextArr);
+fillTextArr.map(elem => {
+  elem.classList.add('fill-font');
+});
+
+
 // https://developer.mozilla.org/ko/docs/Web/API/Canvas_API/Tutorial/Drawing_text
 // todo: 그런데 이 부분을 사용하려면 캔버스 엘리먼트로 바꿔줘야하고 strokeText와 특정 부분의 글씨는 fillText로 나눠 적용해야한다
 // todo: 좌표를 사용해서 글씨를 그려주는 원리인 것 같음
 
 // *fontExplain
 // *div > div*2
+const fontExplainTextObj = {
+  textOne: 'Lorem, ipsum dolor.',
+  textTwo: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat nulla fugit sequi, ut placeat vitae?
+  Quasi perferendis recusandae facilis eaque vero omnis eum, iusto architecto voluptates. Ex sint aperiam sed.
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla aspernatur facilis ratione a culpa dolorem
+  blanditiis quas nisi quia quae. Porro maxime amet tenetur dolorum saepe delectus voluptatum nulla rerum.`
+}
 const fontExplain = document.getElementById('font-explain');
 // console.log(fontExplain);
-fontExplain.innerHTML = `${makeElem('div', 'explain-box', `${makeElem('div', '', 'Lorem, ipsum dolor.')}${makeElem('div', '', `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat nulla fugit sequi, ut placeat vitae?
-Quasi perferendis recusandae facilis eaque vero omnis eum, iusto architecto voluptates. Ex sint aperiam sed.
-Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla aspernatur facilis ratione a culpa dolorem
-blanditiis quas nisi quia quae. Porro maxime amet tenetur dolorum saepe delectus voluptatum nulla rerum.`)}`)}`;
+fontExplain.innerHTML = `${makeElem('div', 'explain-box', `${makeElem('div', '', `${fontExplainTextObj.textOne}`)}${makeElem('div', '', `${fontExplainTextObj.textTwo}`)}`)}`;
+fontExplain.style.color = `${colorObj.colorFf}`;
 
 
 // *qna
@@ -116,6 +130,7 @@ const aTextArr = ['Lorem ipsum dolor sit amet consectetur adipisicing elit. Assu
 // todo: 3개 만들어주기 (ul > li*2)*3
 qnaDataArr.forEach(elem => {
   makePage(qna, 'ul');
+  
 });
 // todo: ul을 그리드로 높이값을 정해주기
 setDisplay(qna, 'grid');
@@ -132,34 +147,29 @@ qnaCon.forEach((elem, index) => {
   elem.classList.add('border-bk');
   // todo: ul마다 각각 > div > li*2
   elem.innerHTML = `${makeElem('div', '', `${makeElem('li', '', `${qTextArr[index]}`)}${makeElem('li', '', `${aTextArr[index]}`)}`)}`;
-  
+
   // todo: 계단식으로 구조를 변경해줄 부분
-  if(index === 0){
+  if (index === 0) {
     // console.log(elem);
     setDisplay(elem, 'flex', 'center', 'flex-start');
-  } else if(index === 1){
+  } else if (index === 1) {
     // console.log(elem);
     setDisplay(elem, 'flex', 'center', 'center');
-  } else{
+  } else {
     // console.log(elem);
     setDisplay(elem, 'flex', 'center', 'flex-end');
   }
-  
+
   // *ul >div
   const textBox = elem.firstElementChild;
   // console.log(textBox);
   textBox.classList.add('border-bk');
   setSize(textBox, `${elem.offsetWidth/3}px`, '');
-  
+
   // *ul > div > li*2(li태그)
   const textItems = Array.from(textBox.children);
   // console.log(textItems);
-  textItems.forEach(elem => {elem.style.listStyleType = `none`;});
-
-
+  textItems.forEach(elem => {
+    elem.style.listStyleType = `none`;
+  });
 });
-// console.log(qna);
-
-
-
-
