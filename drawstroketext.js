14525bce973067e1e72cf8bcc34840d75ebe4070
jieunlee <liz950827@gmail.com>
2022-06-21 11:46:31 +0900

    interaction test

--- drawstroketext.js
+++
@@ -1,5 +0,0 @@
-const draw = (elem, fontSize, fontStyle, text, x, y) => {
-  const context = document.getElementById(`${elem}`);
-  context.font = `${fontSize} ${fontStyle}`;
-  context.strokeText(`${text}`, `${x}`, `${y}`);
-};
