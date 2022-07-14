f22f2720ac4f449450015d6ca5ab349c4ebcad62
jieunlee <liz950827@gmail.com>
2022-07-14 15:13:15 +0900

    새 창으로 링크가 열리도록 적용해줌

---
+++ js/targetblank.js
@@ -0,0 +1,9 @@
+const aTarget = Array.from(document.getElementsByTagName('a'));
+console.log(aTarget);
+// *새 창으로 링크 열리도록 해줌
+aTarget.map(elem => {
+  // *hashchange 적용하지 않은 부분만 적용해줘야 함
+  if(elem.id.includes('#')){
+    elem.target = '_blank';
+  }
+});
